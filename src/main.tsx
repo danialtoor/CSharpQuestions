a9020357a465b84fda1c6ef585369a701481a05c
Danial <dtoor14@gmail.com>
2025-01-01 22:28:51 -0600

    added theme , deleted question component

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+      <ThemeProvider theme={theme}>
     <App />
+  </ThemeProvider>
   </StrictMode>,
 )
