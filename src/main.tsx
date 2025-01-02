f5f1e2de5883f4b6ef55fab7d1f7dbdd64df9c85
Danial <dtoor14@gmail.com>
2025-01-01 22:51:33 -0600

    added axios and react router packages

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from '@mui/material/styles';
+import App from './App';
+import theme from './theme';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-      <ThemeProvider theme={theme}>
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(
+  <ThemeProvider theme={theme}>
     <App />
   </ThemeProvider>
-  </StrictMode>,
-)
+);
